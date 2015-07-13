631919916d83c9405d2395433117046c3bdc301f
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-07-13 23:26:26 +0200

    One more valid example

--- examples/example_cross_correlation.py
+++ examples/example_cross_correlation.py
@@ -1,4 +1,4 @@
-obspy = pr.agent("seis_prov:sa_9DIG8A-TA", other_attributes=(
+obspy = pr.agent("seis_prov:sp001_sa_9dig8ata", other_attributes=(
     ("prov:type",
         prov.identifier.QualifiedName(prov.constants.PROV, "SoftwareAgent")),
     ("prov:label", "ObsPy"),
@@ -8,7 +8,7 @@ obspy = pr.agent("seis_prov:sa_9DIG8A-TA", other_attributes=(
     ("seis_prov:doi", "10.1785/gssrl.81.3.530"))
 )
 
-trace_1 = pr.entity("seis_prov:wf_A34J4DIDJ3", other_attributes=(
+trace_1 = pr.entity("seis_prov:sp002_wf_a34j4didj3", other_attributes=(
     ("prov:label", "Waveform Trace"),
     ("prov:type", "seis_prov:waveform_trace"),
     ("seis_prov:station_id", "BW.FURT.00.BHZ"),
@@ -23,7 +23,7 @@ trace_1 = pr.entity("seis_prov:wf_A34J4DIDJ3", other_attributes=(
     )
 )
 
-trace_2 = pr.entity("seis_prov:wf_0034AIDDJ8", other_attributes=(
+trace_2 = pr.entity("seis_prov:sp003_wf_0034aiddj8", other_attributes=(
     ("prov:label", "Waveform Trace"),
     ("prov:type", "seis_prov:waveform_trace"),
     ("seis_prov:station_id", "BW.ALTM..BHZ"),
@@ -38,21 +38,21 @@ trace_2 = pr.entity("seis_prov:wf_0034AIDDJ8", other_attributes=(
     )
 )
 
-cross_correlate = pr.activity("seis_prov:co_F87SF7SF78", other_attributes=(
+cross_correlate = pr.activity("seis_prov:sp004_co_f87sf7sf78", other_attributes=(
     ("prov:label", "Cross Correlate"),
     ("prov:type", "seis_prov:cross_correlate"),
     ("seis_prov:correlation_type", "Phase Cross Correlation"),
-    ("seis_prov:max_lag_time_in_sec", "120.0")
+    ("seis_prov:max_lag_time_in_sec", 120.0)
 ))
 
-correlation = pr.entity("seis_prov:cc_UTAKGH82HL", other_attributes=(
+correlation = pr.entity("seis_prov:sp005_cc_utakgh82hl", other_attributes=(
     ("prov:label", "Cross Correlation"),
     ("prov:type", "seis_prov:cross_correlation"),
     ("seis_prov:correlation_type", "Phase Cross Correlation"),
     ("seis_prov:station_id_A", "BW.FURT.00.BHZ"),
     ("seis_prov:station_id_B", "BW.ALTM..BHZ"),
-    ("seis_prov:max_correlation_coefficient", "0.75"),
-    ("seis_prov:max_lag_time_in_sec", "120.0")
+    ("seis_prov:max_correlation_coefficient", 0.75),
+    ("seis_prov:max_lag_time_in_sec", 120.0)
 ))
 
 pr.association(cross_correlate, obspy)
